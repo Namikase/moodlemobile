9a084a811a7295362acc1eadc9ae09555936de3f
Namikase <italoivan.rp@gmail.com>
2015-03-13 20:01:14 -0500

    cambios

--- plugins/mootes14/main.js
+++ plugins/mootes14/main.js
@@ -5,6 +5,20 @@ var templates = [
 
 define(templates, function (loginForm, program) {
     var plugin = {
+        settings: {
+            name: "mootes14",
+            type: "general",
+            menuURL: "#mootes14",
+            icon: "plugins/events/icon.png",
+            lang: {
+                component: "core"
+            }
+        },
+
+        routes: [
+            ["mootes14", "show_program", "showProgram"]
+        ],
+
         showProgram: function() {
             var tpl = {};
             var html = MM.tpl.render(program, tpl);
